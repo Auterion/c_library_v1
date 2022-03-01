3bcebfb222eacec92e1c555bebc2dd7d5e857f2b
auterionci <ci@auterion.com>
2022-03-01 16:14:42 +0000

    autogenerated headers for rev https://github.com/auterion/mavlink/tree/07038efb67fd46df3399e83e0eaed1d214acecc7

--- ras_a/mavlink.h
+++ ras_a/mavlink.h
@@ -1,12 +1,12 @@
 /** @file
- *  @brief MAVLink comm protocol built from auterion.xml
+ *  @brief MAVLink comm protocol built from ras_a.xml
  *  @see http://mavlink.org
  */
 #pragma once
 #ifndef MAVLINK_H
 #define MAVLINK_H
 
-#define MAVLINK_PRIMARY_XML_HASH 1685987019284761362
+#define MAVLINK_PRIMARY_XML_HASH -3179073849481490075
 
 #ifndef MAVLINK_STX
 #define MAVLINK_STX 254
@@ -29,6 +29,6 @@
 #endif
 
 #include "version.h"
-#include "auterion.h"
+#include "ras_a.h"
 
 #endif // MAVLINK_H
