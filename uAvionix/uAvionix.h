c67a67620cdae35185cd965477549f9c0e555f70
auterionci <ci@auterion.com>
2022-12-29 17:41:42 +0000

    autogenerated headers for rev https://github.com/auterion/mavlink/tree/d44f6f164d94fdfbf164ac596dcd9659dd523000

--- uAvionix/uAvionix.h
+++ uAvionix/uAvionix.h
@@ -10,7 +10,7 @@
     #error Wrong include order: MAVLINK_UAVIONIX.H MUST NOT BE DIRECTLY USED. Include mavlink.h from the same directory instead or set ALL AND EVERY defines from MAVLINK.H manually accordingly, including the #define MAVLINK_H call.
 #endif
 
-#define MAVLINK_UAVIONIX_XML_HASH 2707651286351042957
+#define MAVLINK_UAVIONIX_XML_HASH 1364925431603389442
 
 #ifdef __cplusplus
 extern "C" {
