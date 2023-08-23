91ffe7325ffd9004559d998f198017daf99c6841
auterionci <ci@auterion.com>
2023-08-23 15:54:40 +0000

    autogenerated headers for rev https://github.com/auterion/mavlink/tree/2eb4da61571d58109bc136a9581c65c516e66a41

--- icarous/icarous.h
+++ icarous/icarous.h
@@ -10,7 +10,7 @@
     #error Wrong include order: MAVLINK_ICAROUS.H MUST NOT BE DIRECTLY USED. Include mavlink.h from the same directory instead or set ALL AND EVERY defines from MAVLINK.H manually accordingly, including the #define MAVLINK_H call.
 #endif
 
-#define MAVLINK_ICAROUS_XML_HASH -4870291747674795777
+#define MAVLINK_ICAROUS_XML_HASH 1879250951090501253
 
 #ifdef __cplusplus
 extern "C" {
