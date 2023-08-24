c4b7d642406435866b54bde6da867805cbce3e22
auterionci <ci@auterion.com>
2023-08-24 18:10:18 +0000

    autogenerated headers for rev https://github.com/auterion/mavlink/tree/97e66bf9e6a9d6b14e4bef6ee42f466843c92007

--- ardupilotmega/ardupilotmega.h
+++ ardupilotmega/ardupilotmega.h
@@ -10,7 +10,7 @@
     #error Wrong include order: MAVLINK_ARDUPILOTMEGA.H MUST NOT BE DIRECTLY USED. Include mavlink.h from the same directory instead or set ALL AND EVERY defines from MAVLINK.H manually accordingly, including the #define MAVLINK_H call.
 #endif
 
-#define MAVLINK_ARDUPILOTMEGA_XML_HASH 3954932132138562152
+#define MAVLINK_ARDUPILOTMEGA_XML_HASH -236556004283329603
 
 #ifdef __cplusplus
 extern "C" {
