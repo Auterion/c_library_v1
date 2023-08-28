f8ddc30faa0240ba6a314c61b8b12b96df4a0bda
auterionci <ci@auterion.com>
2023-08-28 21:25:34 +0000

    autogenerated headers for rev https://github.com/auterion/mavlink/tree/3b4870d1035dcfa3d25e73634133583b98d35861

--- ASLUAV/ASLUAV.h
+++ ASLUAV/ASLUAV.h
@@ -10,7 +10,7 @@
     #error Wrong include order: MAVLINK_ASLUAV.H MUST NOT BE DIRECTLY USED. Include mavlink.h from the same directory instead or set ALL AND EVERY defines from MAVLINK.H manually accordingly, including the #define MAVLINK_H call.
 #endif
 
-#define MAVLINK_ASLUAV_XML_HASH -2733141706956933235
+#define MAVLINK_ASLUAV_XML_HASH 2982612471241238982
 
 #ifdef __cplusplus
 extern "C" {
--- matrixpilot/matrixpilot.h
+++ matrixpilot/matrixpilot.h
@@ -10,7 +10,7 @@
     #error Wrong include order: MAVLINK_MATRIXPILOT.H MUST NOT BE DIRECTLY USED. Include mavlink.h from the same directory instead or set ALL AND EVERY defines from MAVLINK.H manually accordingly, including the #define MAVLINK_H call.
 #endif
 
-#define MAVLINK_MATRIXPILOT_XML_HASH -299485313323411586
+#define MAVLINK_MATRIXPILOT_XML_HASH -8513876484153528875
 
 #ifdef __cplusplus
 extern "C" {
