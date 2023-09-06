0b5dac8f53a456cb866cfd98843442abce04ee89
auterionci <ci@auterion.com>
2023-09-06 08:16:02 +0000

    autogenerated headers for rev https://github.com/auterion/mavlink/tree/3460778c31d392ca29f57d664a1a4dddabf8773f

--- csAirLink/csAirLink.h
+++ csAirLink/csAirLink.h
@@ -10,7 +10,7 @@
     #error Wrong include order: MAVLINK_CSAIRLINK.H MUST NOT BE DIRECTLY USED. Include mavlink.h from the same directory instead or set ALL AND EVERY defines from MAVLINK.H manually accordingly, including the #define MAVLINK_H call.
 #endif
 
-#define MAVLINK_CSAIRLINK_XML_HASH -7079528495323546538
+#define MAVLINK_CSAIRLINK_XML_HASH 7477222906762786007
 
 #ifdef __cplusplus
 extern "C" {
--- paparazzi/paparazzi.h
+++ paparazzi/paparazzi.h
@@ -10,7 +10,7 @@
     #error Wrong include order: MAVLINK_PAPARAZZI.H MUST NOT BE DIRECTLY USED. Include mavlink.h from the same directory instead or set ALL AND EVERY defines from MAVLINK.H manually accordingly, including the #define MAVLINK_H call.
 #endif
 
-#define MAVLINK_PAPARAZZI_XML_HASH 514490593260281524
+#define MAVLINK_PAPARAZZI_XML_HASH 9033990043425402400
 
 #ifdef __cplusplus
 extern "C" {
