9cbba50b471aecfab1e175300dc80589257fa4a7
auterionci <ci@auterion.com>
2023-11-08 16:40:09 +0000

    autogenerated headers for rev https://github.com/auterion/mavlink/tree/fa98ec6bb38bae1ddc4960c90aacd45ea5846bd5

--- auterion/auterion.h
+++ auterion/auterion.h
@@ -10,7 +10,7 @@
     #error Wrong include order: MAVLINK_AUTERION.H MUST NOT BE DIRECTLY USED. Include mavlink.h from the same directory instead or set ALL AND EVERY defines from MAVLINK.H manually accordingly, including the #define MAVLINK_H call.
 #endif
 
-#define MAVLINK_AUTERION_XML_HASH -4373097779560768181
+#define MAVLINK_AUTERION_XML_HASH -3298995573984675358
 
 #ifdef __cplusplus
 extern "C" {
