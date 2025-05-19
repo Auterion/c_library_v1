13c19470027bd9d1801091120349fc5966d9184f
auterionci <ci@auterion.com>
2025-05-19 11:05:05 +0000

    autogenerated headers for rev https://github.com/auterion/mavlink/tree/e9599c15ba628fb27c7df9d04cc1a09e65fd80b5

--- minimal/minimal.h
+++ minimal/minimal.h
@@ -10,7 +10,7 @@
     #error Wrong include order: MAVLINK_MINIMAL.H MUST NOT BE DIRECTLY USED. Include mavlink.h from the same directory instead or set ALL AND EVERY defines from MAVLINK.H manually accordingly, including the #define MAVLINK_H call.
 #endif
 
-#define MAVLINK_MINIMAL_XML_HASH -6013205051356894061
+#define MAVLINK_MINIMAL_XML_HASH 133667410483687834
 
 #ifdef __cplusplus
 extern "C" {
