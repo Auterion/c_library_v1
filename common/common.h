ad845d5f1b5dacbf39a577cdec00942764b71ea4
auterionci <ci@auterion.com>
2025-11-04 13:28:00 +0000

    autogenerated headers for rev https://github.com/auterion/mavlink/tree/4122877e55f38bdcbcaf9e976e6bae4299ab82a3

--- common/common.h
+++ common/common.h
@@ -10,7 +10,7 @@
     #error Wrong include order: MAVLINK_COMMON.H MUST NOT BE DIRECTLY USED. Include mavlink.h from the same directory instead or set ALL AND EVERY defines from MAVLINK.H manually accordingly, including the #define MAVLINK_H call.
 #endif
 
-#define MAVLINK_COMMON_XML_HASH 6255277863948007079
+#define MAVLINK_COMMON_XML_HASH 3815321469886788215
 
 #ifdef __cplusplus
 extern "C" {
@@ -797,7 +797,7 @@ typedef enum MAV_RESULT
 {
    MAV_RESULT_ACCEPTED=0, /* Command is valid (is supported and has valid parameters), and was executed. | */
    MAV_RESULT_TEMPORARILY_REJECTED=1, /* Command is valid, but cannot be executed at this time. This is used to indicate a problem that should be fixed just by waiting (e.g. a state machine is busy, can't arm because have not got GPS lock, etc.). Retrying later should work. | */
-   MAV_RESULT_DENIED=2, /* Command is invalid (is supported but has invalid parameters). Retrying same command and parameters will not work. | */
+   MAV_RESULT_DENIED=2, /* Command is invalid; it is supported but one or more parameter values are invalid (i.e. parameter reserved, value allowed by spec but not supported by flight stack, and so on). Retrying the same command and parameters will not work. | */
    MAV_RESULT_UNSUPPORTED=3, /* Command is not supported (unknown). | */
    MAV_RESULT_FAILED=4, /* Command is valid, but execution has failed. This is used to indicate any non-temporary or unexpected problem, i.e. any problem that must be fixed before the command can succeed/be retried. For example, attempting to write a file when out of memory, attempting to arm when sensors are not calibrated, etc. | */
    MAV_RESULT_IN_PROGRESS=5, /* Command is valid and is being executed. This will be followed by further progress updates, i.e. the component may send further COMMAND_ACK messages with result MAV_RESULT_IN_PROGRESS (at a rate decided by the implementation), and must terminate by sending a COMMAND_ACK message with final result of the operation. The COMMAND_ACK.progress field can be used to indicate the progress of the operation. | */
@@ -2548,6 +2548,17 @@ typedef enum COMPUTER_STATUS_FLAGS
    COMPUTER_STATUS_FLAGS_DISK_FULL=8, /* Indicates if main disk is full. | */
    COMPUTER_STATUS_FLAGS_ENUM_END=9, /*  | */
 } COMPUTER_STATUS_FLAGS;
+#endif
+
+/** @brief Airspeed sensor flags */
+#ifndef HAVE_ENUM_AIRSPEED_SENSOR_FLAGS
+#define HAVE_ENUM_AIRSPEED_SENSOR_FLAGS
+typedef enum AIRSPEED_SENSOR_FLAGS
+{
+   AIRSPEED_SENSOR_UNHEALTHY=1, /* Airspeed sensor is unhealthy | */
+   AIRSPEED_SENSOR_USING=2, /* True if the data from this sensor is being actively used by the flight controller for guidance, navigation or control. | */
+   AIRSPEED_SENSOR_FLAGS_ENUM_END=3, /*  | */
+} AIRSPEED_SENSOR_FLAGS;
 #endif
 
 // MAVLINK VERSION
