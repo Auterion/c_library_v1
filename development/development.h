bdba2d7369dec031bb6d8d528032e8c26aacd1e4
auterionci <ci@auterion.com>
2026-05-06 12:07:29 +0000

    autogenerated headers for rev https://github.com/auterion/mavlink/tree/f830691a207f9954a1006d156487c93523e99705

--- development/development.h
+++ development/development.h
@@ -10,7 +10,7 @@
     #error Wrong include order: MAVLINK_DEVELOPMENT.H MUST NOT BE DIRECTLY USED. Include mavlink.h from the same directory instead or set ALL AND EVERY defines from MAVLINK.H manually accordingly, including the #define MAVLINK_H call.
 #endif
 
-#define MAVLINK_DEVELOPMENT_XML_HASH 7520137708691420761
+#define MAVLINK_DEVELOPMENT_XML_HASH 1273216876987900277
 
 #ifdef __cplusplus
 extern "C" {
